192f654f6033aec7c152d06027fe01fa5c888839
Thomas Paino <thomas@paino.cloud>
2016-07-05 16:12:51 +0200

    merge manuale

--- script_runner_test.go
+++ script_runner_test.go
@@ -11,7 +11,7 @@ var exp_res_runner string = "Hello World\n"
 func TestRunner(t *testing.T) {
 	str := []string{"Hello", "World"}
 	wd, _ := os.Getwd()
-	fpath := filepath.Join(wd, "data", "example.sh")
+	fpath := filepath.Join(wd, "scripts", "hello.sh")
 	out := Runner(fpath, str)
 	if exp_res_runner != out {
 		t.Error("Expected Hello World, got", out)
