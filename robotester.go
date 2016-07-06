e648168efe657f474e189002a7c62af5b7983464
Thomas Paino <thomas@paino.cloud>
2016-07-06 17:08:15 +0200

    cambiata sintassi e riscritta funzione di parsing per le query

--- robotester.go
+++ robotester.go
@@ -7,30 +7,59 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
+
+// GetArgs parses a URL and returns a slice of strings maintaining the order of
+// keys and values
+func GetArgs(u *url.URL) []string {
+	q := strings.Split(u.String(), "?")[1]
+	order := strings.Split(q, "&")
+
+	var opts []string
+	for _, v := range order {
+		if v != "" {
+			if strings.ContainsAny(v, "=") {
+				t := strings.SplitN(v, "=", 2)
+				key := t[0]
+				value := t[1]
+				opts = append(opts, key)
+
+				if value != "" {
+					opts = append(opts, value)
+				}
+			} else {
+				opts = append(opts, v)
+			}
+		}
+	}
+	return opts
+}
 
 // RunScript parses the API request and extracts the script name and arguments
 // with the appropriate checks.
 // The request must have a 'script' field with the name of the script to call and
 // optional 'arg' fields.
-// Example: => script.py foo -bar 42
-// 			=> run?script=script.py&arg=foo&arg=-bar&arg=42
+// Example: script.py foo -bar 42
+// 			=> /run/script.py?foo=-bar&42
+//			=> /run/script.py?foo&-bar=42
 // If the request is valid, the script dispatcher module is called.
 func RunScript(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Responding to /run request from %s\n", r.Host)
 
-	r.ParseForm()
+	queries := mux.Vars(r)
 	w.WriteHeader(http.StatusOK)
-	script := r.FormValue("script")
+	script := queries["script"]
 
 	if script != "" {
 		if hasScript(script) {
 
-			args := r.Form["arg"]
+			args := GetArgs(r.URL)
 			uuid := NewHash()
 			wd, _ := os.Getwd()
 			path := filepath.Join(wd, DIR_SCRIPTS, script)
@@ -38,8 +67,8 @@ func RunScript(w http.ResponseWriter, r *http.Request) {
 
 			w.Write([]byte(fmt.Sprintf("Script: %s\n", script)))
 			w.Write([]byte(fmt.Sprintf("Full Path: %s\n", path)))
-			w.Write([]byte(fmt.Sprintf("Options: %s\n", args)))
 			w.Write([]byte(fmt.Sprintf("UUID: %s\n", uuid)))
+
 			fmt.Fprintln(w, "\n==== BEGIN OUTPUT ====")
 			w.Write([]byte(out))
 			fmt.Fprintln(w, "==== END OUTPUT ====")
@@ -62,7 +91,7 @@ func main() {
 
 	// init http handlers
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/run", RunScript).
+	router.HandleFunc("/run/{script}", RunScript).
 		Methods("GET")
 
 	// start http server
