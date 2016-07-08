d191982599f8c4f1d9b15287f3c42cedab54d110
Thomas Paino <thomas@paino.cloud>
2016-07-08 17:23:13 +0200

    aggiunta generazione struct e canale di coda per richieste run

--- daemon.go
+++ daemon.go
@@ -6,13 +6,49 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/satori/go.uuid"
 )
 
+type Job struct {
+	script  string
+	path    string
+	args    []string
+	uuid    string
+	output  string
+	exit    string
+	request time.Time
+	start   time.Time
+	finish  time.Time
+}
+
+var jobQueue chan Job
+
 func RunHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("I handle /run requests!\n"))
+	LogAppendLine(fmt.Sprintf("Responding to /run request from %s", r.Host))
+	vars := mux.Vars(r)
+	script := vars["script"]
+
+	if hasScript(script) {
+		r.ParseForm()
+		wd, _ := os.Getwd()
+
+		job := Job{
+			script: script,
+			args:   r.Form["args"],
+			uuid:   uuid.NewV4().String(),
+			path:   filepath.Join(wd, DIR_SCRIPTS, script)}
+
+		jobQueue <- job
+		fmt.Fprintf(w, "QUEUED with uuid: %s", job.uuid)
+
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "ERROR  no script named %s found in %s", script, DIR_SCRIPTS)
+	}
 }
 
 func LogHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +57,10 @@ func LogHandler(w http.ResponseWriter, r *http.Request) {
 
 func StatusHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("I handle /status requests!\n"))
+}
+
+func init() {
+	jobQueue = make(chan Job, 10)
 }
 
 func main() {
--- runner.go
+++ runner.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"io/ioutil"
+	"strings"
+)
+
+// default scripts directory
+var DIR_SCRIPTS string = "scripts"
+
+// hasScript checks for the script existance
+func hasScript(search string) bool {
+	files, err := ioutil.ReadDir(DIR_SCRIPTS)
+	if err == nil {
+		k := len(files)
+		for i := 0; i < k; i++ {
+			if strings.Contains(files[i].Name(), search) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// listScripts returns a list of scripts in the default script folder
+func listScripts() []string {
+	files, err := ioutil.ReadDir(DIR_SCRIPTS)
+	if err == nil {
+		names := []string{}
+		k := len(files)
+		for i := 0; i < k; i++ {
+			names = append(names, files[i].Name())
+		}
+		return names
+	}
+	return nil
+}
