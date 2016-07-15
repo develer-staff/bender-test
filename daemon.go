da90170b6650f3d7b1d47d010e5161784d6e45ea
Thomas Paino <thomas@paino.cloud>
2016-07-15 15:38:11 +0200

    implemented runner module, changed methods handling of context

--- daemon.go
+++ daemon.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net/http"
@@ -22,6 +23,10 @@ const (
 	JOB_COMPLETED  = "completed"
 )
 
+func (jobStatus JobStatus) ToJson() ([]byte, error) {
+	return json.Marshal(jobStatus)
+}
+
 type Job struct {
 	Script  string    `json:"script"`
 	Path    string    `json:"path"`
@@ -35,10 +40,14 @@ type Job struct {
 	Status  JobStatus `json:"status"`
 }
 
+func (job Job) ToJson() ([]byte, error) {
+	return json.Marshal(job)
+}
+
 type appContext struct {
 	ScriptsDir string
-	JobQueue   chan Job
-	JobDone    chan Job
+	JobQueue   chan *Job
+	JobDone    chan *Job
 }
 
 type cmdArgs struct {
@@ -68,15 +77,48 @@ func parseArgs() *cmdArgs {
 func (c *cmdArgs) initAppContext() *appContext {
 	context := &appContext{
 		ScriptsDir: *c.scriptsDir,
-		JobQueue:   make(chan Job, *c.jobQueueSize),
-		JobDone:    make(chan Job)}
+		JobQueue:   make(chan *Job, *c.jobQueueSize),
+		JobDone:    make(chan *Job)}
 	return context
 }
 
 // RunHandler handles /run requests
 func (a *appContext) RunHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	fmt.Fprintf(w, "Requested execution of script '%s'\n", vars["script"])
+
+	// parse http request query
+	r.ParseForm()
+	script := vars["script"]
+	args := r.Form["args"]
+	requested := time.Now()
+
+	// Validate request and build job
+	job := NewJob(a, script, args, requested)
+
+	if job.Status == JOB_NOT_FOUND {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "No script %s in %s found", job.Script, job.Path)
+		return
+	}
+
+	// Send to queue
+	job.Status = JOB_QUEUED
+	select {
+	case a.JobQueue <- job:
+	default:
+		LogAppendLine("SERVER  job queue is full")
+		w.WriteHeader(http.StatusServiceUnavailable)
+		fmt.Fprintf(w, "Queue is full: cannot accept job %s", job.Uuid)
+		return
+	}
+
+	// respond with json file
+	js, err := job.ToJson()
+	if err != nil {
+		panic(err.Error())
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
 
 // LogHandler handles /log requests
@@ -113,6 +155,9 @@ func main() {
 	router.HandleFunc("/log/script/{script}", context.LogHandler).Methods("GET")
 	router.HandleFunc("/log/uuid/{uuid}", context.LogHandler).Methods("GET")
 	router.HandleFunc("/status", context.StatusHandler).Methods("GET")
+
+	// init runwoker
+	go RunWorker(context)
 
 	// start http server
 	LogAppendLine(fmt.Sprintf("SERVER  listening on port %d", *args.serverPort))
